71f9c1c819bf6c5e2f6ccfd5f976a6b84c6cabbc
Dipin Jassal <71757022+DipinJassal@users.noreply.github.com>
2020-11-08 17:48:27 +0530

    Update script.js

--- script.js
+++ script.js
@@ -18,10 +18,10 @@ function addTodo(e) {
     todoDiv.classList.add("todo");
 
     
-    if(todoInput.value.length == 0){   //This will return an alert if the input is empty.
-        alert("Please enter an item!")
-        return;
-    }    
+    //if(todoInput.value.length == 0){   //This will return an alert if the input is empty.
+    //    alert("Please enter an item!")
+   //     return;
+   // }    
     
     if(todoInput.value == 0){
         alert("Please enter an item!")
